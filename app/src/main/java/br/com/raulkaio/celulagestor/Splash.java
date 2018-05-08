aaa154e5f11a8e52782f94134f3fb6260f89cb9f
Raul Kaio <rkaio18@gmail.com>
2018-05-08 10:39:58 -0300

    Initial commit

---
+++ app/src/main/java/br/com/raulkaio/celulagestor/Splash.java
@@ -0,0 +1,23 @@
+package br.com.raulkaio.celulagestor;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity implements Runnable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 2000);
+    }
+
+    public void run() {
+        startActivity(new Intent(Splash.this, MainActivity.class));
+        finish();
+    }
+}
